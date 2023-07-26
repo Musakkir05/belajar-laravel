48b5ee01b4a59b4072f8219c8b8ff409018d7064
Musakkir05 <sakkirypf@gmail.com>
2023-07-26 18:43:16 +0800

    perubahan Pada Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,5 +4,5 @@
 
 @section('content')
     <h1>ini adalah halaman home</h1>
-    <h3>halo, Selamat datang {{Auth::user()->name}}. Anda adalah {{Auth::user()->role->name}}</h3>
+    <h3>Halo, Welcome   {{Auth::user()->name}}. Anda adalah {{Auth::user()->role->name}}</h3>
 @endsection
